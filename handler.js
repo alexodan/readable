e0a269172340630b03519455dd2fb03d8de933c8
Alexis Diaz <alexdan1993@gmail.com>
2024-03-05 17:42:38 -0300

    cors and http

--- handler.js
+++ handler.js
@@ -14,12 +14,19 @@ module.exports.hello = async (event) => {
   };
 };
 
-module.exports.read = async (event) => {
+module.exports.read = async (event, _context, callback) => {
   const { getArticle } = require("./readability");
   const url = event.queryStringParameters.url;
   const article = await getArticle(url);
-  return {
+
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(article, null, 2),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(article),
   };
+
+  callback(null, response);
 };
